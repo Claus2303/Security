c3a246332410169cc0879b7b52551a63afbae54d
claus <claus@test.de>
2020-11-10 21:54:11 +0100

    EFA00000003 Server Logger eingebaut

--- src/main/java/de/claus/ssl/server/SSLServer.java
+++ src/main/java/de/claus/ssl/server/SSLServer.java
@@ -10,8 +10,14 @@ import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocket;
 
-public class SSLServer implements Runnable{
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import de.claus.ssl.client.SSLClient;
+
+public class SSLServer implements Runnable{
+	private static final Logger logger = LogManager.getLogger(SSLServer.class);
+    
 	/**
 		-Djavax.net.debug=all 
 		-Djavax.net.ssl.keyStore="C:\Wks\Git\Security\src\main\config\mykeystore.p12" 
@@ -21,10 +27,9 @@ public class SSLServer implements Runnable{
         try {
             SSLServerSocket serverSocket = (SSLServerSocket) SSLServerSocketFactory
                     .getDefault().createServerSocket(5678);
-            System.out.println("Server ready..." + serverSocket);
+            logger.debug("Server ready..." + serverSocket);
 
-            System.out
-                    .println("Supported Cipher Suites: "
+            logger.debug("Supported Cipher Suites: "
                             + Arrays.toString(((SSLServerSocketFactory) SSLServerSocketFactory
                                             .getDefault())
                                             .getSupportedCipherSuites()));
@@ -32,12 +37,12 @@ public class SSLServer implements Runnable{
             SSLSocket socket = (SSLSocket) serverSocket.accept();
             SSLSession sslSession = socket.getSession();
             String cipherSuite = sslSession.getCipherSuite();
-            System.out.println(cipherSuite);
+            logger.info(cipherSuite);
 
             Scanner scanner = new Scanner(socket.getInputStream());
-            System.out.println("Reading...");
+            logger.debug("Reading...");
             while (scanner.hasNextLine()) {
-                System.out.println("Server received: " + scanner.nextLine());
+            	logger.debug("Server received: " + scanner.nextLine());
             }
             scanner.close();
 
@@ -45,7 +50,7 @@ public class SSLServer implements Runnable{
 
             serverSocket.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error(e);
         }
     }
     public static void main(String[] args) {
